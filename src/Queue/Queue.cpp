6bf85b7cdf7d065348ddea39e54adbbac0fcf6a8
Primeeight <zacharywray@gmail.com>
2023-06-08 14:19:45 -0400

    6/8/23

--- src/Queue/Queue.cpp
+++ src/Queue/Queue.cpp
@@ -7,44 +7,72 @@
 // ex. std::cout
 //using namespace std;
 
-template <class T>
-Queue<T>::Queue () = default;
+template<class T>
+Queue<T>::Queue() = default;
 
-template <class T>
-Queue<T>::~Queue () = default;
+template<class T>
+Queue<T>::~Queue() = default;
 
-template <class T>
-void Queue<T>::clear () {
+template<class T>
+void Queue<T>::clear() {
+    _seq.clear();
 
 }
 
+/**
+ * Transfer the source queue to queue class
+ * clear out source
+ * @tparam T
+ * @param source
+ */
 template<class T>
 void Queue<T>::transferFrom(Queue<T> &source) {
-
+    _seq.transferFrom(source);
 }
 
 template<class T>
-Queue<T> & Queue<T>::operator=(Queue<T> const& rhs) = default;
+Queue<T> &Queue<T>::operator=(Queue<T> const &rhs) = default;
 
 template<class T>
 void Queue<T>::enqueue(T &x) {
-_seq.add(x, 0);
+    _seq.add(x, 0);
 }
 
 template<class T>
 void Queue<T>::dequeue(T &x) {
-_seq.remove(x, _seq.length());
+    _seq.remove(x, _seq.length());
 
 }
 
+/**
+ * If the list is empty, return the list
+ * Replace the front element with x
+ * Place the removed element in x
+ * Generic type @tparam T temp
+ * return @param x
+ */
 template<class T>
-void Queue<T>::replaceFront(T& x) {
+void Queue<T>::replaceFront(T &x) {
+    if (length() == 0) {
+        return;
+    } else {
+        enqueue(x);
+        T temp = dequeue(0);
+        x = temp;
+        return x;
+    }
 
 }
 
+/**
+ * TODO
+ * Return an optional generic type for the front of the queue.
+ * @tparam T
+ * @return
+ */
 template<class T>
 std::optional<T> Queue<T>::front() {
-   return std::nullopt;
+    return _seq.entry(0);
 }
 
 template<class T>
@@ -54,7 +82,7 @@ int Queue<T>::length() {
 
 template<class T>
 std::string Queue<T>::outputQueue() {
-    return " ";
+    return _seq.outputSequence();
 }
 
 
--- src/Sequence/Sequence.h
+++ src/Sequence/Sequence.h
@@ -8,7 +8,7 @@
  * Description:
  * @tparam T Generic type
  */
-template <class T>
+template<class T>
 class Sequence {
 
 private:
@@ -27,7 +27,7 @@ private:
         /**
          * Description: Points at the next NodeRecord in the sequence
          */
-        NodeRecord* next;
+        NodeRecord *next;
 
         NodeRecord() = default;
     };
@@ -35,7 +35,7 @@ private:
     /**
      * Description: First value in sequence
      */
-    NodeRecord* head;
+    NodeRecord *head;
 
     /**
     * Description: size of sequence
@@ -46,65 +46,65 @@ private:
      * Description: Reclaims all space in the heap from the allocated for p and any sequential NodeRecord it points to.
      * @param p A NodeRecord value
      */
-    void reclaimAllNodes (NodeRecord*& p);
+    void reclaimAllNodes(NodeRecord *&p);
 
 public:
 
     /**
      * Description: Constructor that initializes all attributes of this class.
      */
-    Sequence ();
+    Sequence();
 
     /**
      * Description: Deconstructor that reclaims all space allocated to the heap.
      */
-    ~Sequence ();
+    ~Sequence();
 
     /**
      * Description: Clears all of the elements in the data structure.
      */
-    void clear ();
+    void clear();
 
     /**
     * Description: Transfers all values from source and clears out source
     * @param source TODO Fill out what this parameter is
     *
     */
-    void transferFrom(Sequence& source);
+    void transferFrom(Sequence &source);
 
     /**
    * Description: Transfer elements from one sequence to another
    * @param source Sequence to get elements from
    *
    */
-    Sequence& operator=(Sequence const& rhs);
+    Sequence &operator=(Sequence const &rhs);
 
-        /**
-      * Description: Adds a node to sequence
-      * @param x value to place in node field
-      * @param pos index in sequence representing location to add node
-      */
-    void add (T& x, int pos);
+    /**
+  * Description: Adds a node to sequence
+  * @param x value to place in node field
+  * @param pos index in sequence representing location to add node
+  */
+    void add(T &x, int pos);
 
     /**
     * Description: Removes a node from sequence
     * @param x value removed from node
     * @param pos index in sequence representing location of node
     */
-    void remove (T& x, int pos);
+    void remove(T &x, int pos);
 
     /**
     * Description: Gets value at pos
     * @param pos index in sequence representing location of node
     * @return value from given position
     */
-    std::optional<T> entry (int pos);
+    std::optional<T> entry(int pos);
 
     /**
     * Description: size of sequence
     * @return number of nodes in sequence
     */
-    int length ();
+    int length();
 
     /**
      * Description: prints out values in sequence
